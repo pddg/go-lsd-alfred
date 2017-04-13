e1645ec3146dca669b4f8d8c098d768679632923
pddg <b4111018@edu.kit.ac.jp>
2017-04-13 15:59:40 +0900

    refactored

--- scraping/scrape.go
+++ scraping/scrape.go
@@ -6,14 +6,9 @@ import (
 )
 
 func ScrapeMeaning(doc *goquery.Document, response *[]models.ResponseItem) error {
-	base_url := "https://lsd-project.jp"
 	doc.Find("div.caption").Each(func(_ int, caption *goquery.Selection) {
 		subtitle := caption.Find("span.headword").Text()
-		meanings := caption.Next()
-		// if next section is "relword" or something different from "meanings", search "meanings"
-		if !meanings.HasClass("meaning") {
-			meanings = meanings.Next()
-		}
+		meanings := findMeaning(caption)
 		explanation := meanings.Find("span.explanation").First().Text()
 		meanings.Find("span.headword").Each(func(_ int, meaning *goquery.Selection) {
 			item := new(models.ResponseItem)
@@ -21,28 +16,24 @@ func ScrapeMeaning(doc *goquery.Document, response *[]models.ResponseItem) error
 			if len(explanation) != 0 {
 				item.Text.Largetype = explanation
 			}
-			meaning_a := meaning.Find("a")
-			var headword string
-			if meaning_a != nil {
-				headword = meaning_a.Text()
-				item.Title, item.Text.Copy, item.Autocomplete = headword, headword, headword
-				url, link_exists := meaning_a.Attr("href")
-				if link_exists {
-					item.Arg = headword
-					item.Valid = true
-					// when press `shift` key
-					item.Mod.Shift.Arg = base_url + url
-					item.Mod.Shift.Valid = true
-					item.Mod.Shift.Subtitle = "Open: " + base_url + url
-				}
-			} else {
-				headword = meaning.Text()
-				item.Title, item.Text.Copy, item.Autocomplete = headword, headword, headword
-			}
+			headword, url := findHeadword(meaning)
+			item.Title, item.Text.Copy, item.Autocomplete, item.Arg = headword, headword, headword, headword
+			item.Valid = true
+			url = BaseUrl + url
+			// when press `shift` key
+			createModItem(
+				item,
+				ShiftKey,
+				"Open: "+url,
+				url,
+				true)
 			// when press `cmd` key
-			item.Mod.Cmd.Valid = true
-			item.Mod.Cmd.Subtitle = "Copy '" + headword + "' to Clipboard"
-			item.Mod.Cmd.Arg = headword
+			createModItem(
+				item,
+				CommandKey,
+				"Copy '"+headword+"' to Clipboard",
+				headword,
+				true)
 			*response = append(*response, *item)
 		})
 	})
@@ -52,4 +43,29 @@ func ScrapeMeaning(doc *goquery.Document, response *[]models.ResponseItem) error
 		*response = append(*response, *item)
 	}
 	return nil
+}
+
+func findMeaning(caption *goquery.Selection) *goquery.Selection {
+	meanings := caption.Next()
+	// if next section is "relword" or something different from "meanings", search "meanings"
+	if !meanings.HasClass("meaning") {
+		meanings = meanings.Next()
+	}
+	return meanings
+}
+
+func findHeadword(meaning *goquery.Selection) (string, string) {
+	var (
+		headword string
+		url      string
+	)
+	if meaning_a := meaning.Find("a"); meaning_a != nil {
+		headword = meaning_a.Text()
+		if u, exists := meaning_a.Attr("href"); exists {
+			url = u
+		}
+	} else {
+		headword = meaning.Text()
+	}
+	return headword, url
 }
--- scraping/util.go
+++ scraping/util.go
@@ -4,10 +4,15 @@ import (
 	"github.com/PuerkitoBio/goquery"
 	"github.com/pddg/go-lsd-alfred/models"
 	"golang.org/x/text/unicode/norm"
+	"strings"
 )
 
+const BaseUrl string = "https://lsd-project.jp"
+const ShiftKey string = "shift"
+const CommandKey string = "cmd"
+
 func GetPage(mode string, query string) (*goquery.Document, error) {
-	return goquery.NewDocument("https://lsd-project.jp/weblsd/" + NormalizeQuery(mode+"/"+query))
+	return goquery.NewDocument(BaseUrl + "/weblsd/" + NormalizeQuery(mode+"/"+query))
 }
 
 func CreateError(err error, msg string) *models.ResponseItem {
@@ -19,15 +24,26 @@ func CreateError(err error, msg string) *models.ResponseItem {
 }
 
 func CreateOrigin(mode string, query string) *models.ResponseItem {
-	url := "https://lsd-project.jp/weblsd/" + NormalizeQuery(mode+"/"+query)
+	url := BaseUrl + "/weblsd/" + NormalizeQuery(mode+"/"+query)
 	item := new(models.ResponseItem)
 	item.Title = "See all results in web site ..."
 	item.Subtitle = "Please input `Shift` + `Enter`"
 	item.Arg = url
-	item.Mod.Shift.Arg = url
-	item.Mod.Shift.Valid = true
-	item.Mod.Shift.Subtitle = "Open: " + url
+	createModItem(item, ShiftKey, "Open: "+url, url, true)
 	return item
+}
+
+func createModItem(item *models.ResponseItem, keyname string, subtitle string, arg string, valid bool) {
+	switch strings.ToLower(keyname) {
+	case ShiftKey:
+		item.Mod.Shift.Subtitle = subtitle
+		item.Mod.Shift.Arg = arg
+		item.Mod.Shift.Valid = valid
+	case CommandKey:
+		item.Mod.Cmd.Subtitle = subtitle
+		item.Mod.Cmd.Arg = arg
+		item.Mod.Cmd.Valid = valid
+	}
 }
 
 func NormalizeQuery(query string) string {
